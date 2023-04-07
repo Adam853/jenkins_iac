0c254b58cd32a9c1ba5c7688f6f08f786baadc99
Adam853 <83390423+Adam853@users.noreply.github.com>
2023-04-07 11:50:23 +0200

    Update jenkins_iac.groovy

--- jenkins_iac.groovy
+++ jenkins_iac.groovy
@@ -1,1 +1,16 @@
+import groovy.json.JsonSlurper
+
+//configuration root path
+def config_root_path = "${WORKSPACE}/jenkins_job"
+
+//liux tree comand
+def tree_command = "tree -J ${config_root_path}
+
+
+//get root tree of jobs config in json format
+def getJsonConfigTree(command) {
+  def tree = ['bash' , '-c', "${command}"].execute().text
+  return new JsonSlurper().parseText(tree)
+}
+
 println "This is message from DSL job groovy script file!"
